5b965e99aef93a6f0555d862527496990995921b
Sourab Maity <62734958+sourabmaity@users.noreply.github.com>
2022-09-26 21:58:55 +0530

    to get the value of QRcode

---
+++ RaspberryPi/qrCodeScanner.py
@@ -0,0 +1,21 @@
+import time
+import cv2
+
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+
+
+picam2.start()
+time.sleep(2)
+detector = cv2.QRCodeDetector()
+while True:
+    img=picam2.capture_array()
+    data, bbox, _ = detector.detectAndDecode(img)
+    print(str(data))
+    cv2.imshow("Frame",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+picam2.close()
+cv2.destroyAllWindows()
