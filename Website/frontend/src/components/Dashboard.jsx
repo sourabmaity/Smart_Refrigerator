4cb5eb2750e72dfc5d02549bc029ad87da1f2f5c
Sudip Das <61116990+iconicsudip@users.noreply.github.com>
2022-10-24 19:37:31 +0530

    front-end updated

--- Website/frontend/src/components/Dashboard.jsx
+++ Website/frontend/src/components/Dashboard.jsx
@@ -25,18 +25,28 @@ export default function Dashboard() {
   const handleChange = React.useCallback((newValue) => {
     setOpen(newValue);
   }, []);
-  const toSearch = async (e)=>{
-    e.preventDefault();
-    if(e.target.value==="" || String(e.target.value).indexOf("#")!==-1){
-      setSearchResult(null)
+  const toSearch = async (e,item=false)=>{
+    let final_item = "";
+    if(e.target!==undefined){
+      final_item = e.target.value;
+    }else{
+      final_item = e
     }
-    setSearch(e.target.value);
+    if(final_item===""){
+      setSearch(null)
+    }
+    if(final_item===""){
+      setSearch('');
+    }else{
+      setSearch(final_item);
+    }
     // setSearchResult(null)
     setLoader(true)
-    if(e.target.value!==""){
-      await fetch("http://127.0.0.1:8000/api/gosearch/",{
+    if(final_item!==""){
+      
+      await fetch("https://smrtfrze.herokuapp.com/api/gosearch/",{
           method:'POST',
-          body: JSON.stringify({item:e.target.value}),
+          body: JSON.stringify({item:final_item}),
           headers:{
               'Content-Type':'application/json',
               'Authorization':'Bearer '+ String(authToken.access),
@@ -49,12 +59,27 @@ export default function Dashboard() {
           }
           setLoader(false);
       })
+    }else{
+      setSearchResult(null)
+      setLoader(false);
     }
   }
   const getItem = (e)=>{
-    console.log("Hui")
+    var selected_item = e.target.innerHTML.split('<b>')[1].split('<span')[0].split('</b>').join('')
+    setSearch(selected_item)
+    toSearch(selected_item)
   }
-  console.log(search);
+  const searchItems = (e)=>{
+    e.preventDefault();
+    console.log("Searching...")
+    setTimeout(() => {
+      console.log(search)
+    }, 3000);
+    
+  }
+  const eraseList = (e)=>{
+    setSearchResult(null)
+  }
   return (
     <div className='allbody dashboard'>
         {Alert}
@@ -66,30 +91,33 @@ export default function Dashboard() {
             </Typography>
           </Box>
         </Modal>
-        <form className="dashboard-search-form form-inline align-center my-2 my-lg-0 d-inline w-100">
+        <form className="dashboard-search-form form-inline align-center my-2 my-lg-0 d-inline w-100" onSubmit={searchItems}>
           <TextField className='search-box' value={search} onChange={toSearch} size="small" id="fullWidth" label="Search Recipe" variant="outlined" autoComplete='false'/>
           {searchresult?
-          <div className="search_result">
-            {loader?
-            <div className="loader" style={{marginTop:0}}>
-                <img src="./loading.gif" width={40} alt="" />
-            </div>:
-            Array.isArray(searchresult)?
-            <List>
-              {searchresult.map((sr,ind)=>{
-                  return(
-                    <ListItem disablePadding>
-                        <ListItemButton onClick={getItem}>
-                            <li  key={"item"+ind}><b>{search.charAt(0).toUpperCase() + search.slice(1)}</b>{sr.substring(search.length)}</li>
-                        </ListItemButton>
-                    </ListItem>
-                )
-              })}
-            </List>
-            :searchresult}
-          </div>
+            <div className="search_result">
+              {loader?
+              <div className="loader" style={{marginTop:0}}>
+                  <img src="./loading.gif" width={40} alt="" />
+              </div>:
+              <div className="close-items">
+                <span class="close" onClick={eraseList}>&times;</span>
+                {Array.isArray(searchresult)?
+                  <List style={{width:"100%"}}>
+                    {searchresult.map((sr,ind)=>{
+                      return (
+                        <ListItem disablePadding onClick={getItem} key={"item"+ind}>
+                          <ListItemButton >
+                            <b>{search.charAt(0).toUpperCase() + search.slice(1)}</b>{sr.substring(search.length)}
+                          </ListItemButton>
+                        </ListItem>
+                      )
+                    })}
+                  </List>
+                :<span style={{textAlign:"center",width: "100%"}}>{searchresult}</span>}
+              </div>}
+            </div>
           :null}
-          <Button className='search-item' onClick={()=>{console.log("Hello")}} variant="contained">Search</Button>
+          <Button className='search-item' onClick={searchItems} variant="contained">Search</Button>
         </form>
     </div>
   )
